34095cc72bc3ccc2d71266cf66b3d065f7427101
PRATHAMESH <prathameshp800@gmail.com>
2023-10-06 18:16:51 +0530

    project_initialized

---
+++ lib/expenses.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+  @override
+  State<Expenses> createState() {
+    return ExpensesState();
+  }
+}
+
+class ExpensesState extends State<Expenses> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
